b326884ea2ab3f62f9a47cbe3c7716090169b7b0
Ivan Chernykh <ivanchernykh98@gmail.com>
2025-12-19 18:01:40 +0200

    feat: super basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PitchDetector } from "pitchy";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const audioRef = useRef<AudioContext>(null);
+  const analyserRef = useRef<AnalyserNode>(null);
+  const detectorRef =
+    useRef<PitchDetector<Float32Array<ArrayBufferLike>>>(null);
+  const inputRef = useRef<Float32Array<ArrayBuffer>>(null);
+  const rafRef = useRef<number>(null);
+
+  const [pitch, setPitch] = useState(0);
+  const [clarity, setClarity] = useState(0);
+  const [isListening, setIsListening] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      const audio = new AudioContext();
+      const analyser = audio.createAnalyser();
+      analyser.fftSize = 2048;
+
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      audio.createMediaStreamSource(stream).connect(analyser);
+
+      const detector = PitchDetector.forFloat32Array(analyser.fftSize);
+      detector.minVolumeDecibels = -25;
+      const input = new Float32Array(detector.inputLength);
+
+      audioRef.current = audio;
+      analyserRef.current = analyser;
+      detectorRef.current = detector;
+      inputRef.current = input;
+    };
+
+    init();
+  }, []);
+
+  const start = () => {
+    setIsListening(true);
+    audioRef.current?.resume();
+
+    const loop = () => {
+      analyserRef.current!.getFloatTimeDomainData(inputRef.current!);
+      const [p, c] = detectorRef.current!.findPitch(
+        inputRef.current!,
+        audioRef.current!.sampleRate
+      );
+      setPitch(p);
+      setClarity(c);
+
+      rafRef.current = requestAnimationFrame(loop);
+    };
+
+    loop();
+  };
+
+  const stop = () => {
+    if (rafRef.current) {
+      cancelAnimationFrame(rafRef.current);
+    }
+    audioRef.current?.suspend();
+    setPitch(0);
+    setClarity(0);
+    setIsListening(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center">
+        <div>Pitch: {Math.round(pitch * 10) / 10} HZ</div>
+        <div>Clarity: {Math.round(clarity * 100)}</div>
+        <div className="mt-2 flex justify-center gap-2">
+          <button
+            onClick={start}
+            className="bg-blue-400 hover:bg-blue-500 enabled:hover:cursor-pointer rounded p-2 disabled:bg-blue-200"
+            disabled={isListening}
+          >
+            start recording
+          </button>
+          <button
+            onClick={stop}
+            className="bg-blue-400 hover:bg-blue-500 enabled:hover:cursor-pointer rounded p-2 disabled:bg-blue-200"
+            disabled={!isListening}
+          >
+            stop recording
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
